e358b69263eca92fa15d7153a3380e26447242e2
Ardiantohendy <bayuardianto421@gmail.com>
2023-10-03 21:56:13 +0700

    new commit

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import GoToHome from "./pages/GoToHome";
 import GoToAbout from "./pages/GoToAbout";
 import GoToFilm from "./pages/GoToFilm";
+import FilmographyPage from "./components/FilmographyPage";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<GoToHome />} />
-        <Route path="/about" element={<GoToAbout />} />
-        <Route path="/filmography" element={<GoToFilm />} />
-      </Routes>
-    </Router>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<GoToHome />} />
+    //     <Route path="/about" element={<GoToAbout />} />
+    //     <Route path="/filmography" element={<GoToFilm />} />
+    //   </Routes>
+    // </Router>
+    <FilmographyPage />
   );
 }
 
